ad4649f01e222c2b883e201441f7b45f6e3f0efc
Med-Marwen-BS <92266158+Med-Marwen-BS@users.noreply.github.com>
2021-11-09 13:20:54 +0100

    admin+authentif(-template)

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
 ];
