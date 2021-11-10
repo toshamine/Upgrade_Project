0a305d678bc3c65a0519f582cc02c77627161c1e
Med-Marwen-BS <92266158+Med-Marwen-BS@users.noreply.github.com>
2021-11-10 16:59:23 +0100

    admin+authentif(-template)

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\Category;
 use App\Entity\Certification;
+use App\Entity\User1;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -29,6 +30,8 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linktoRoute('Back to the website', 'fas fa-home', 'index');
+        yield MenuItem::linkToCrud('Users', 'fas fa-list', User1::class);
         yield MenuItem::linkToCrud('Certification', 'fas fa-list', Certification::class);
         yield MenuItem::linkToCrud('Categorie', 'fas fa-list', Category::class);
 
