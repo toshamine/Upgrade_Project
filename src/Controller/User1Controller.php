992c9cfb6fbdc8ff8b9751d8d15155fd7abfa086
Med-Marwen-BS <92266158+Med-Marwen-BS@users.noreply.github.com>
2021-11-30 22:37:10 +0100

    profile - edit

--- src/Controller/User1Controller.php
+++ src/Controller/User1Controller.php
@@ -13,30 +13,7 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/user1')]
 class User1Controller extends AbstractController
 {
-    #[Route('/', name: 'user1_index', methods: ['GET'])]
-    public function index(User1Repository $user1Repository): Response
-    {
-        if($this->getUser()){
 
-            $entityManager = $this->getDoctrine()->getManager();
-            $userV = $entityManager->getRepository(User1::class)->findOneBy(['email'=>$this->getUser()->getUserIdentifier(),
-                'isVerified'=>false]);
-
-            if ($userV)
-            {
-                return $this->redirectToRoute('app_logout');
-            }
-
-
-            return $this->render("user1/index.html.twig",['user'=>$this->getUser()
-                ,'user1s' => $user1Repository->findAll(),]);
-        }
-
-
-        return $this->render('user1/index.html.twig', [
-            'user1s' => $user1Repository->findAll(),
-        ]);
-    }
 
     /**
      * @Route("/myprofile",name="profile")
@@ -47,40 +24,16 @@ class User1Controller extends AbstractController
         // $user1 = $this->getUser();
         $entityManager = $this->getDoctrine()->getManager();
         $user1 = $entityManager->getRepository(User1::class)->findOneBy(['email'=>$this->getUser()->getUserIdentifier()]);
-
+        $picture = $this->getParameter("app.path.product_images").'/'.$user1->getPicture();
         return $this->render('user1/show.html.twig', [
-            'user1' => $this->getUser(),'user' => $this->getUser()
+            'user1' => $this->getUser(),'user' => $this->getUser(),
+            'pic'=>$picture,
         ]);
     }
 
-    #[Route('/new', name: 'user1_new', methods: ['GET','POST'])]
-    public function new(Request $request): Response
-    {
-        $user1 = new User1();
-        $form = $this->createForm(User1Type::class, $user1);
-        $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($user1);
-            $entityManager->flush();
 
-            return $this->redirectToRoute('user1_index', [], Response::HTTP_SEE_OTHER);
-        }
 
-        return $this->renderForm('user1/new.html.twig', [
-            'user1' => $user1,
-            'form' => $form,
-        ]);
-    }
-
-    #[Route('/{id}', name: 'user1_show', methods: ['GET'])]
-    public function show(User1 $user1): Response
-    {
-        return $this->render('user1/show.html.twig', [
-            'user1' => $user1,
-        ]);
-    }
 
     #[Route('/{id}/edit', name: 'user1_edit', methods: ['GET','POST'])]
     public function edit(Request $request, User1 $user1): Response
@@ -97,6 +50,7 @@ class User1Controller extends AbstractController
         return $this->renderForm('user1/edit.html.twig', [
             'user1' => $user1,
             'form' => $form,
+            'user'=>$this->getUser(),
         ]);
     }
 
