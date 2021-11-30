d440e0750003fd421e525e8dd41ff82c0e5a6aa6
Amine <toshamine@gmail.com>
2021-11-30 20:17:32 +0100

    Instructors V0

---
+++ src/Controller/InstructorsController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User1;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class InstructorsController extends AbstractController
+{
+    /**
+     * @Route ("/listinstructors",name="listinstructors")
+     */
+   public function listinstructors():Response
+   {
+       $instructors=$this->getDoctrine()->getRepository(User1::class)->findAll();
+       $finalist=array();
+       foreach ($instructors as $in) {
+           if (in_array("ROLE_MANAGER", $in->getRoles())) {
+               array_push($finalist,$in );
+           }
+       }
+       return $this->render("Instructors/instructors.html.twig",['instructors'=>$finalist]);
+   }
+}
