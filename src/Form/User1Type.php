2dd94543df2d0554ceea21ac00c7265a4d9f04b9
Med-Marwen-BS <92266158+Med-Marwen-BS@users.noreply.github.com>
2021-12-02 14:04:52 +0100

    profile edit profile

--- src/Form/User1Type.php
+++ src/Form/User1Type.php
@@ -5,8 +5,10 @@ namespace App\Form;
 use App\Entity\User1;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
 use Vich\UploaderBundle\Form\Type\VichImageType;
 use function Sodium\add;
 
@@ -22,6 +24,26 @@ class User1Type extends AbstractType
            // ->add('imageFile', VichImageType::class,[
             //    'attr' => ['class' => 'rounded-circle mt-5','width'=>'150px'],
            // ])
+           ->add('imageFile', FileType::class, [
+               //'label' => '',
+
+               // unmapped means that this field is not associated to any entity property
+               //'mapped' => false,
+
+               // make it optional so you don't have to re-upload the PDF file
+               // every time you edit the Product details
+               'required' => false,
+
+               // unmapped fields can't define their validation using annotations
+               // in the associated entity, so you can use the PHP constraint classes
+               'constraints' => [
+                   new File([
+                       'maxSize' => '1024k',
+
+                       'mimeTypesMessage' => 'Please upload a valid picture',
+                   ])
+               ],
+           ])
             ->add('FirstName')
             ->add('LastName')
             ->add('Birthdate',  DateType::class, [
